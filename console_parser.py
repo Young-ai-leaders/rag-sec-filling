e6d310826350ba2f8da372217477a6347bafa317
nabilpradl <62774659+nabilpradl@users.noreply.github.com>
2025-05-09 10:53:07 +0200

    Directory
    Easier for git ignore

--- console_parser.py
+++ console_parser.py
@@ -6,7 +6,7 @@ def run_parser_console(ticker: str) -> None:
 
     confirm = input(f"\nDo you want to parse and split all filings in '{filings_dir}'? (y/n): ").strip().lower()
     if confirm == "y":
-        output_file = f"parsed_{ticker}_structured.json"
+        output_file = f"./output/parsed_{ticker}_structured.json"
         parser.parse_all_filings_structured(output_file=output_file)  
         print(f"\nExported structured parsed sections to {output_file}")
     else:
