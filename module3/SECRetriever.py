61d9420eb2a919b23c8f30728e1e28b5b2d60d48
Florian Hitzler <43441416+florian4300@users.noreply.github.com>
2025-10-25 13:05:49 +0200

    added Retriever LangChain class

---
+++ module3/SECRetriever.py
@@ -0,0 +1,28 @@
+from typing import Collection
+
+from langchain_core.documents import Document
+from langchain_core.retrievers import BaseRetriever
+from typing import Any
+from module2.core.search_service import vector_search_with_filter
+
+
+class SECRetriever(BaseRetriever):
+    collection: Any
+    search_index_name: str
+    model: Any
+    tokenizer: Any
+    k: int = 5
+
+
+    def _get_relevant_documents(self, query: str, filters=None) -> list[Document]:
+        """Return the first k documents from the list of documents"""
+        results = vector_search_with_filter(
+            collection=self.collection,
+            index_name=self.search_index_name,
+            query_text=query,
+            model=self.model,
+            tokenizer=self.tokenizer,
+            limit=self.k,
+            filters=filters
+        )
+        return results
